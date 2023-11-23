53f8fc22b244bb0e8b4d3d6fad199430d370257f
Paco <pacop0587@gmail.com>
2023-11-22 22:24:00 -0600

    Se agrega validacion para inicio de sesion

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def signup(request):
     """Funcion signup"""
     contexto = {'form':UserCreationForm}
-    return render(request, 'signup.html',contexto)
+    print(request.method)
+    if request.method == 'GET':
+        return render(request, 'signup.html',contexto)
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado')
+            except:
+                return HttpResponse('El usuario ya existe')
+        return HttpResponse('No coinciden las contraseñas')
